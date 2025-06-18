31d2550398b243136c69be7aa296eac2e11c846a
Harsh4902 <harshparmar4902@gmail.com>
2025-06-18 15:45:14 +0530

    Basic Configuration and DB connection

---
+++ src/main/java/org/example/bitespeedbackendexample/BitespeedBackendExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.bitespeedbackendexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitespeedBackendExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BitespeedBackendExampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/bitespeedbackendexample/BitespeedBackendExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bitespeedbackendexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitespeedBackendExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
